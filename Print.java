3c75a9800576ece64815a563abbf4cb484e12d84
S-HariNarayan <harinarayan.s2024cce@sece.ac.in>
2025-07-09 09:30:18 +0530

    Add files via upload

---
+++ Print.java
@@ -0,0 +1,10 @@
+public class Print
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hari");
+        System.out.print("Yashas ");
+        System.out.println("Abhisheik");
+        System.out.println("Ranjith");
+    }
+}
